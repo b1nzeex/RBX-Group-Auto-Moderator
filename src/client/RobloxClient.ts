6efbca5e5f40cff6ea0b162546eff443e2fd48ca
b1nzeex <cbeachx@gmail.com>
2023-05-25 13:17:48 +0100

    upd: Tidied up a bit of code

--- src/client/RobloxClient.ts
+++ src/client/RobloxClient.ts
@@ -21,14 +21,11 @@ export default class RobloxClient {
 
     // We iterate through the posts.
     for (const post of posts) {
-      // We check if the post contains a URL.
-      if (StringUtils.hasUrl(post.body)) {
-        // We delete the post.
-        await RequestUtils.deletePost(this.groupId, post.id);
-      }
-
-      // We check if the post contains a bad phrase.
-      if (StringUtils.hasBadPhrase(post.body)) {
+      // We check if the post contains a URL or bad phrase.
+      if (
+        StringUtils.hasUrl(post.body) ||
+        StringUtils.hasBadPhrase(post.body)
+      ) {
         // We delete the post.
         await RequestUtils.deletePost(this.groupId, post.id);
       }
